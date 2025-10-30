a78910a2a94bc56abc75250f1a4ebb95af7a1794
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-10-30 23:15:24 +0300

    add POST method and also add thank_you template for redirection when the form is a post

--- FEEDBACK/settings.py
+++ FEEDBACK/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-xxgrsi8sxhe^xr0pchqk1-8c97@k75je!&njznkguu7y09^jw%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    'testserver',
+]
 
 
 # Application definition
@@ -37,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews.apps.ReviewsConfig',
 ]
 
 MIDDLEWARE = [
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+def review(request):
+    if request.method == "POST":
+        entered_username = request.POST.get("username")
+        print(f"Username entered: {entered_username}")
+        return HttpResponseRedirect("/thank-you/")
+    return render(request, 'reviews/review.html')
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
+
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
