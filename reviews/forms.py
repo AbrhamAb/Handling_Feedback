337e4bd6c541c303bbbe1ec8e8f056f0a532dba1
Abrham Abebaw <abrhamabebaw.tech@gmail.com>
2025-10-31 14:45:23 +0300

    integrate database to form and use modelform to generate the form automatically

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,4 +1,17 @@
 from django import forms
+from .models import Review
+# class ReviewForm(forms.Form):
+#     username = forms.CharField(max_length=100, label='Your Name', widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}),error_messages={'required': 'Please enter your name.'})
+#     review_text = forms.CharField(label='Your Feedback', widget=forms.Textarea(attrs={'placeholder': 'Write your review here...'}), error_messages={'required': 'Please write a review.'})
+#     rating = forms.IntegerField(label='Rating (1-5)', min_value=1, max_value=5, error_messages={'required': 'Please provide a rating between 1 and 5.', 'min_value': 'Rating must be at least 1.', 'max_value': 'Rating cannot be more than 5.'})
 
-class ReviewForm(forms.Form):
-    username = forms.CharField(max_length=100, label='Your Name', widget=forms.TextInput(attrs={'placeholder': 'Enter your name'}),error_messages={'required': 'Please enter your name.'})
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model= Review
+        fields = '__all__'
+
+        labels = {
+            'username': 'Your Name',
+            'review_text': 'Your Feedback',
+            'rating': 'Rating (1-5)',
+        }
--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import ReviewForm   
+from .forms import ReviewForm  
+from .models import Review 
 # Create your views here.
+def thank_you(request):
+    return render(request, 'reviews/thank_you.html')
 
 def review(request):
     if request.method == "POST":
@@ -9,24 +12,13 @@ def review(request):
 
 
         if form.is_valid():
+            review = Review(username=form.cleaned_data['username'],
+                            review_text=form.cleaned_data['review_text'],
+                            rating=form.cleaned_data['rating'])
+            review.save()
             return HttpResponseRedirect("/thank-you/")
             
     else:
         form = ReviewForm()
     return render(request, 'reviews/review.html', {"form": form})
 
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
-
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
-
-
-    form = ReviewForm()
-    return render(request, 'reviews/review.html', {"form": form})
-
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
-
-def thank_you(request):
-    return render(request, 'reviews/thank_you.html')
